d2a09e0e9940e29211c1d2c4ccdda67a4bc06206
gmarik <gmarik+git@gmail.com>
2021-03-13 23:18:30 -0800

    scratch.src: helloworld-asm

---
+++ scratch.src/helloworld-asm/helloworld.s
@@ -0,0 +1,14 @@
+.data
+  hello:
+    .string "Hello, assembly!"
+
+.text
+  .global main 
+  main:
+    push {ip, lr}
+    ldr r0, =hello
+    bl printf
+    mov r0, #41
+    add r0, r0, #1 // Increment
+    pop {ip, lr}
+    bx lr
